f09e4084bf6fed0a6bdc3511dcd28895562a53a9
swadianosogen <swadianosogen0@gmail.com>
2026-05-08 10:43:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const CACHE = 'photopolaroid-v1';
+const BASE = '/PhotopolaroidPro/';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([BASE, BASE + 'index.html'])));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
